2f1977035572df8c1ce6841fbcc6f6728c93dfd5
Stephan-MDD <stephan.mdd@gmail.com>
2020-03-26 14:08:48 +0100

    changed port to 5000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const socket = require('./scripts/socket');
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 5000;
 
 app.get('/', (req, res) => res.send('Hello HackChat!'));
 const server = app.listen(port, () => console.log(`Server listening on port ${port}! @ http://localhost:${port}/`));
