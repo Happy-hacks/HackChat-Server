437fbcf6459cc401d0272a842e463d194182457c
Stephan-MDD <stephan.mdd@gmail.com>
2020-03-29 19:19:32 +0200

    added environment references

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const socket = require('./scripts/socket');
 const express = require('express');
-const app = express();
-const port = 4000;
+require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT;
 app.get('/', (req, res) => res.send('Hello HackChat!'));
 const server = app.listen(port, () => console.log(`Server listening on port ${port}! @ http://localhost:${port}/`));
 
