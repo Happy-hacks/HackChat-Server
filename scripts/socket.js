33e2af756c1291dddc52574e5100702a76707871
Stephan-MDD <stephan.mdd@gmail.com>
2020-04-05 16:34:46 +0200

    fixes

--- scripts/socket.js
+++ scripts/socket.js
@@ -3,11 +3,21 @@ const socket = require('socket.io');
 function connect(server) {
 	const io = socket(server);
 
+	const messages = [];
+
 	io.on('connection', (socket) => {
 		console.log('made socket connection', socket.id);
 
+		console.log(socket.handshake.query.handle);
+
 		socket.on('chat', (data) => {
-			io.sockets.emit('chat', data);
+			const message = { ...data, time: new Date().getTime() };
+
+			// appending new messages to list, max amount is set to 10
+			if (messages.length > 10) messages.pop();
+			messages.push(message);
+
+			io.sockets.emit('chat', message);
 		});
 
 		socket.on('typing', (data) => {
