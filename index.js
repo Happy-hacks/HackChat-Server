352000e55efcf60c85341c6aac9f58c4c25e309a
Stephan-MDD <stephan.mdd@gmail.com>
2020-04-05 15:00:57 +0200

    added authentication with dummy users

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const socket = require('./scripts/socket');
 const express = require('express');
+const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => res.send('Hello HackChat!'));
+
+app.post('/authenticate', (req, res) => {
+	const { handle, password } = req.body;
+
+	// should to be from database
+	const users = {
+		marc: 'pass',
+		tobi: 'pass'
+	};
+
+	// proxy validation
+	if (users[handle] === password) res.send({ handle });
+	else res.status(404).send({ error: 'login for this username and password is invalid' });
+});
+
 const server = app.listen(port, () => console.log(`Server listening on port ${port}! @ http://localhost:${port}/`));
 
 socket(server);
